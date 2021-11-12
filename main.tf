b34f20f2384613e8fbcdb2b96ad47e0c05742603
Pradeepta Kumar Sahu <psahu@nasuni.com>
2021-11-12 15:41:26 +0530

    Add correct boto3 package to NACManager

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_instance" "NACManager" {
       "which terraform",
       "sudo apt install python3 -y",
       "sudo apt install python3-pip -y",
-      "sudo python3 -m pip install --user boto3",
+      "sudo pip3 install boto3",
       "curl 'https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip' -o 'awscliv2.zip'",
       "sudo unzip awscliv2.zip",
       "sudo ./aws/install",
