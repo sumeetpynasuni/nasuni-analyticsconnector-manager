fb9eb5831826020b7bfb9c8871dc3c9dfd865453
Pradeepta Kumar Sahu <psahu@nasuni.com>
2022-02-09 18:38:57 +0530

    Security Group Update for NAC Scheduler

--- main.tf
+++ main.tf
@@ -32,44 +32,6 @@ resource "aws_instance" "NACScheduler" {
   #   volume_size = 101
   # }
 
- provisioner "remote-exec" {
-    inline = [
-      "sudo apt update",
-      "sudo apt upgrade -y",
-      "sudo apt install dos2unix",
-      "sudo apt install curl bash ca-certificates git openssl wget vim -y",
-      "curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -",
-      "sudo apt-add-repository \"deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main\"",
-      "sudo apt update",
-      "sudo apt install terraform",
-      "terraform -v",
-      "which terraform",
-      "sudo apt install jq -y",
-      "sudo apt install unzip",
-      "sudo apt install python3 -y",
-      "sudo apt install python3-pip -y",
-      "sudo pip3 install boto3",
-      "curl 'https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip' -o 'awscliv2.zip'",
-      "sudo unzip awscliv2.zip",
-      "sudo ./aws/install",
-      "aws --version",
-      "which aws",
-      "aws configure --profile ${var.aws_profile} set aws_access_key_id ${data.local_file.aws_conf_access_key.content}",
-      "aws configure --profile ${var.aws_profile} set aws_secret_access_key ${data.local_file.aws_conf_secret_key.content}",
-      "aws configure set region ${data.aws_region.current.name} --profile ${var.aws_profile}",
-      "sudo apt update",
-      "ls",
-      "echo '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ FINISH @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'"
-      ]
-  }
-
-  connection {
-    type        = "ssh"
-    host        = self.public_ip
-    user        = "ubuntu"
-    private_key = file("./${var.pem_key_file}")
-  }
-
   tags = {
     Name = var.nac_scheduler_name
   }
@@ -78,6 +40,14 @@ depends_on = [
   data.local_file.aws_conf_access_key,
   data.local_file.aws_conf_secret_key,
 ]
+}
+
+
+resource "null_resource" "update_secGrp" {
+  provisioner "local-exec" {
+     command = "sh update_secGrp.sh ${aws_instance.NACScheduler.public_ip} ${var.nac_scheduler_name} ${data.aws_region.current.name} ${var.aws_profile} "
+  }
+  depends_on = [aws_instance.NACScheduler]
 }
 
 resource "null_resource" "NACScheduler_IP" {
@@ -112,31 +82,74 @@ data "local_file" "aws_conf_secret_key" {
   depends_on = [null_resource.aws_conf]
 }
 
+ resource "null_resource" "Inatall_Packages" {
+ provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt upgrade -y",
+      "sudo apt install dos2unix",
+      "sudo apt install curl bash ca-certificates git openssl wget vim -y",
+      "curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -",
+      "sudo apt-add-repository \"deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main\"",
+      "sudo apt update",
+      "sudo apt install terraform",
+      "terraform -v",
+      "which terraform",
+      "sudo apt install jq -y",
+      "sudo apt install unzip",
+      "sudo apt install python3 -y",
+      "sudo apt install python3-pip -y",
+      "sudo pip3 install boto3",
+      "curl 'https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip' -o 'awscliv2.zip'",
+      "sudo unzip awscliv2.zip",
+      "sudo ./aws/install",
+      "aws --version",
+      "which aws",
+      "aws configure --profile ${var.aws_profile} set aws_access_key_id ${data.local_file.aws_conf_access_key.content}",
+      "aws configure --profile ${var.aws_profile} set aws_secret_access_key ${data.local_file.aws_conf_secret_key.content}",
+      "aws configure set region ${data.aws_region.current.name} --profile ${var.aws_profile}",
+      "sudo apt update",
+      "ls",
+      "echo '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ FINISH @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'"
+      ]
+  }
 
-# resource "null_resource" "Inatall_APACHE" {
-#  provisioner "remote-exec" {
-#     inline = [
-#       "sudo apt update",
-#       "echo '@@@@@@--------------------------- START ----------------------------------@@@@@@@@'",
-#       "sudo apt install apache2 -y",
-#       "sudo ufw app list",
-#       "sudo ufw allow 'Apache'",
-#       "sudo service apache2 restart",
-#       "sudo service apache2 staus",
+  connection {
+    type        = "ssh"
+    host        = aws_instance.NACScheduler.public_ip
+    user        = "ubuntu"
+    private_key = file("./${var.pem_key_file}")
+  }
+  depends_on = [null_resource.update_secGrp]
+ }
 
-#       "echo '@@@@@@--------------------------- FINISH ----------------------------------@@@@@@@@'"
-#       ]
-#   }
-#   connection {
-#     type        = "ssh"
-#     host        = aws_instance.NACScheduler.public_ip
-#     user        = "ubuntu"
-#     private_key = file("./${var.pem_key_file}")
-#     # private_key = file("./${var.aws_key_name[data.aws_region.current.name]}.pem")
-#   }
-#   depends_on = [aws_instance.NACScheduler]
-# }
-
+resource "null_resource" "Inatall_APACHE" {
+ provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "echo '@@@@@@--------------------------- START ----------------------------------@@@@@@@@'",
+      "sudo apt install apache2 -y",
+      "sudo ufw app list",
+      "sudo ufw allow 'Apache'",
+      "sudo service apache2 restart",
+      "sudo systemctl apache2 staus",
+      "git clone https://github.com/psahuNasuni/SearchUI.git",
+      "cd SearchUI/SearchUI_Web",
+      "sudo chmod 755 /var/www/html/*",
+      "echo '@@@@@@--------------------------- FINISH ----------------------------------@@@@@@@@'"
+      ]
+  }
+  connection {
+    type        = "ssh"
+    host        = aws_instance.NACScheduler.public_ip
+    user        = "ubuntu"
+    private_key = file("./${var.pem_key_file}")
+    # private_key = file("./${var.aws_key_name[data.aws_region.current.name]}.pem")
+  }
+  depends_on = [null_resource.Inatall_Packages]
+}
+      # "cp index.html search.js style.css /var/www/html/",
+      # "sudo service apache2 restart",
 
 output "NACScheduler_ip" {
   value = "${aws_instance.NACScheduler.public_ip}"
